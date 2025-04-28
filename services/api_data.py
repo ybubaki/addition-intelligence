a128a323aac8350bdedb1b52b758a8d7030ae293
buks <ybubaki@gmail.com>
2025-04-28 16:57:39 +0000

    remove print

--- services/api_data.py
+++ services/api_data.py
@@ -6,10 +6,10 @@ def get_company_firmographics(ai_code_or_isin: str):
     try:
         url = f"{_BASE_URL}/firmographics/{ai_code_or_isin}"
         response = requests.get(url)
-        print(response.json())
+        # print(response.json())
         return response.json()
     except Exception as e:
-        print(e)
+        # print(e)
         return None
 
 
@@ -17,9 +17,9 @@ def get_company_ownership(ai_code_or_isin: str):
     try:
         url = f"{_BASE_URL}/ownership/{ai_code_or_isin}"
         response = requests.get(url)
-        print(response.json())
+        # print(response.json())
         return response.json()
     except Exception as e:
-        print(e)
+        # print(e)
         return None
 
